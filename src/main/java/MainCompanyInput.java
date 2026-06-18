8d3f9867716a5ef4438603f49641952d20503592
Irina <irinafengshui71@gmail.com>
2026-06-18 16:03:31 +0200

    HW_17_06_26-

--- src/main/java/MainCompanyInput.java
+++ src/main/java/MainCompanyInput.java
@@ -2,15 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MainCompanyInput {
     public static void main(String[] args){
 
         ArrayList<Employee> company = new ArrayList<>();
-        Employee emp = getEmployee();
-        company.add(emp);
-        printCompany(company);
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader scan = new BufferedReader(is);
+        while (true) {
+            Employee emp = getEmployee();
+            company.add(emp);
 
+            System.out.print("another employee? yes or no>>>");
+            String answer = null;
+            try {
+                answer = scan.readLine();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+            if (answer.equalsIgnoreCase("no"))
+            break;
+            //scan.close();
+        }
+        printCompany(company);
     }
 
     private static void printCompany(ArrayList<Employee> company) {
@@ -35,7 +50,6 @@ public class MainCompanyInput {
             str = br.readLine();
             double salary = Double.parseDouble(str);
             emp = new Employee(name, id, position, salary);
-
 
         }catch (IOException e){
             System.out.println(e.getMessage());
